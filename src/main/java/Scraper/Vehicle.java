c3907d82232ae2636f565b8f8d599a5f3736c1eb
Mark Evans <markleighevans@sky.com>
2015-12-14 21:24:45 +0000

    Initial

--- src/main/java/Scraper/Vehicle.java
+++ src/main/java/Scraper/Vehicle.java
@@ -4,12 +4,39 @@ import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.*;
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 /**
  * Created by mark on 14/12/15.
  */
 public class Vehicle {
+    private String VRM;
+    private String Make;
+    private String Model;
+    private String Colour;
+    private String FirstReg;
+    private String MOTDate;
+    private String TaxDate;
+    private String  Weight;
+
+
+    public String GetVRM()
+    {
+        return this.VRM;
+    }
+    public void SetVRM(String VRM)
+    {
+        this.VRM = VRM;
+    }
+    public String GetMake()
+    {
+        return this.Make;
+    }
+    public void SetMake(String Make)
+    {
+        this.Make = Make;
+    }
 public static void PopulateFromHTML (String VRM) throws IOException
     {
 
