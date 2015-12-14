a2287a38245c53d1d19c48f2a26bae09125c78c3
Mark Evans <markleighevans@sky.com>
2015-12-14 20:59:02 +0000

    Initial

--- src/main/java/Scraper/App.java
+++ src/main/java/Scraper/App.java
@@ -1,5 +1,7 @@
 package Scraper;
 
+import java.io.IOException;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        try
+        {
+            Vehicle.PopulateFromHTML("WF10EDK");
+        }
+        catch(IOException IOE)
+        {
+
+        };
     }
 }
