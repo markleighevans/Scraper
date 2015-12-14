420acb9dfb101f93c7c5078b2c6f0e2d178bf782
Mark Evans <markleighevans@sky.com>
2015-12-14 20:11:42 +0000

    Initial commit

---
+++ src/main/java/Scraper/App.java
@@ -0,0 +1,13 @@
+package Scraper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
